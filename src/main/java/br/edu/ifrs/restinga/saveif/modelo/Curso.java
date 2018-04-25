d3350832081a061a68b134350397ca72d6dcc8c4
giordano <buffonggb@gmail.com>
2018-04-25 04:44:46 -0300

    Ajustes para cadastro de usuario com curso

--- src/main/java/br/edu/ifrs/restinga/saveif/modelo/Curso.java
+++ src/main/java/br/edu/ifrs/restinga/saveif/modelo/Curso.java
@@ -1,43 +1,43 @@
 package br.edu.ifrs.restinga.saveif.modelo;
 
+import javax.persistence.*;
 import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
-public class Curso implements Serializable{
-    
+public class Curso implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id; 
-  
-    @Column(nullable = false, length=80)
-    private String nome;   
-    
-    @Column(nullable = false, length=80)
+    private int id;
+
+    @Column(nullable = false, length = 80)
+    private String nome;
+
+    @Column(nullable = false, length = 80)
     private String nivel;
 
 
     // Contrutores que permitem o cadastro e exclusao de usuarios.
     // Verificar validade dos mesmos.
 
-    Curso(String id){
-        this.id = Integer.parseInt(id);
+    Curso(String id) {
+        if (!id.equals("")) {
+            this.id = Integer.parseInt(id);
+        }
     }
 
-    Curso(){
+    Curso() {
 
     }
 
     public int getId() {
         return id;
     }
+
     public String getNome() {
         return nome;
     }
+
     public String getNivel() {
         return nivel;
     }
@@ -46,17 +46,14 @@ public class Curso implements Serializable{
     public void setId(int id) {
         this.id = id;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public void setNivel(String nivel) {
         this.nivel = nivel;
     }
 
-    
-    
-    
-    
-    
-    
+
 }
