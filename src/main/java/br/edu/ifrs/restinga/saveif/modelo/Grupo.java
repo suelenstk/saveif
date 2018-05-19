06b2a18950f3369251fa654ba660549cdb25e9e3
giordano <buffonggb@gmail.com>
2018-05-19 19:31:01 -0300

    Validacao tipo de imagem

--- src/main/java/br/edu/ifrs/restinga/saveif/modelo/Grupo.java
+++ src/main/java/br/edu/ifrs/restinga/saveif/modelo/Grupo.java
@@ -3,24 +3,11 @@ package br.edu.ifrs.restinga.saveif.modelo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @Entity
 public class Grupo implements Serializable {
@@ -176,11 +163,7 @@ public class Grupo implements Serializable {
     }
 
     public void setTipoImagem(String tipoImagem) throws Exception {
-        System.out.println("tipoImagem "+tipoImagem);
-//        int num = tipoImagem.split(".").length;
-//        System.out.println("num "+num);
         String name = tipoImagem.split("/")[1].toLowerCase();
-        System.out.println("name "+name);
         if (name.equals("png") || name.equals("tiff") || name.equals("jpg") || name.equals("jpeg") || name.equals("bmp")) {
             this.tipoImagem = tipoImagem;
         } else {
